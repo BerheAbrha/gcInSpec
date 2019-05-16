b53b74cd1fb920d039abf4b536b2a5911b2bb96a
Michael Greene <migreene@microsoft.com>
2019-05-16 14:50:16 -0500

    installation script

---
+++ install_inspec.ps1
@@ -0,0 +1,57 @@
+#requires –runasadministrator
+# 
+# This script installs Chef Inspec on Windows
+# 
+
+$Script:InSpec_Version = [version]'4.3.2.1'
+
+function Get-InstalledInSpecVersions {
+    [cmdletbinding()]
+    param()
+
+    Write-Output "[$((get-date).getdatetimeformats()[45])] Checking for InSpec..."
+    
+    $Installed_InSpec = Get-CimInstance win32_product -Filter "Name LIKE 'InSpec%'"
+    $Installed_InSpec_Versions = $Installed_InSpec | ForEach-Object { $_.Version }
+    $Installed_InSpec = if ($null -eq $Installed_InSpec_Versions) { $false } else { $true }
+    
+    $returnStatus = New-Object -TypeName PSObject -ArgumentList @{
+        Installed = $Installed_InSpec
+        Versions  = $Installed_InSpec_Versions
+    }
+    return $returnStatus
+}
+
+function Install-Inspec {
+    [cmdletbinding()]
+    param()
+    
+    if ($false -eq (Get-InstalledInSpecVersions).Installed) {
+        $InSpec_Package_Version = "$($InSpec_Version.Major).$($InSpec_Version.Minor).$($InSpec_Version.Build)"
+        $Inspec_Package_Name = "inspec-$InSpec_Package_Version-$($InSpec_Version.Revision)-x64.msi"
+        $Inspec_Download_Uri = "https://packages.chef.io/files/stable/inspec/$InSpec_Package_Version/windows/2016/$Inspec_Package_Name"
+        
+        Write-Output "[$((get-date).getdatetimeformats()[45])] Downloading InSpec to $pwd\$Inspec_Package_Name"
+        Invoke-WebRequest -Uri $Inspec_Download_Uri -TimeoutSec 120 -RetryIntervalSec 5 -MaximumRetryCount 12 -OutFile "$pwd\$Inspec_Package_Name"
+        
+        $msiArguments = @(
+            "/i"
+            ('"{0}"' -f "$pwd\$Inspec_Package_Name")
+            "/qn"
+        )
+        Write-Output "[$((get-date).getdatetimeformats()[45])] Installing InSpec with arguments: $msiArguments"
+        Start-Process "C:\Windows\System32\msiexec.exe" -ArgumentList $msiArguments -Wait -NoNewWindow
+        Write-Output "[$((get-date).getdatetimeformats()[45])] InSpec installation process ended"
+    }
+}
+
+Install-Inspec
+
+$status = Get-InstalledInSpecVersions
+
+if ($false -eq $status.Installed) {
+    throw "[$((get-date).getdatetimeformats()[45])] A failure occured and InSpec could not be installed."
+}
+else {
+    Write-Output "[$((get-date).getdatetimeformats()[45])] InSpec versions available on this machine: $($status.Versions)."
+}
